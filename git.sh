febe0a445f22562b10a9cdb10a005840b24d77e8
zhengJ_docnet <f60_100002959292_weather@git.cloud.tencent.com>
2019-12-10 17:34:40 +0800

    架构搭建初步完成

--- git.sh
+++ git.sh
@@ -2,7 +2,9 @@ unset msg
 
 read -p "请输入commit提交的描述: " msg
 
+git init
 git add -A
 git commit -m $msg
-git push
+git remote add origin https://github.com/singgl/vue-admin.git
+git push -u origin master
 git status
